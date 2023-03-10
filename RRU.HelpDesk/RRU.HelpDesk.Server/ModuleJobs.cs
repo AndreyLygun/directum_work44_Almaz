29ac5659a18cc70498af1424c6c479e03e2d5971
Andrey Lygun <lygun@yandex.ru>
2023-03-10 19:23:49 +0300

    Выполнено задание 13 [Rx]

--- RRU.HelpDesk/RRU.HelpDesk.Server/ModuleJobs.cs
+++ RRU.HelpDesk/RRU.HelpDesk.Server/ModuleJobs.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -9,5 +9,19 @@ namespace RRU.HelpDesk.Server
   public class ModuleJobs
   {
 
+    /// <summary>
+    /// 
+    /// </summary>
+    public virtual void SendRequestCountInfo()
+    {
+      var performer = Sungero.Company.Employees.GetAll(e => e.Name.Contains("Лыгун")).FirstOrDefault();
+      if (performer != null) {
+        var count = Requests.GetAll().Count();
+        var todaycount = Requests.GetAll(r => r.CreatedDate.Equals(Calendar.Today)).Count();
+        var info = Sungero.Workflow.SimpleTasks.CreateWithNotices("Уведомление о количество сообщений", performer);
+        info.ActiveText = RRU.HelpDesk.Resources.RequestCountInfoFormat(count, todaycount);
+        info.Start();
+      }
+    }
   }
 }
